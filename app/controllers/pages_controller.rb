2ffec3a07c5ccd0613918072928923431e0cdf3f
Deji Hastrup <deji.hastrup@gmail.com>
2023-09-25 18:09:57 +0100

    routes, pages and actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def training
+  end
+
+  def about
+  end
+
+  def blog
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'training', to: 'pages#training'
+  get 'about', to: 'pages#about'
+  get 'blog', to: 'pages#blog'
+  get 'contact', to: 'pages#contact'
 end
