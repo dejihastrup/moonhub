e75d7505f03c7bb0f8a1939b8fabf2ab509c6114
Deji Hastrup <deji.hastrup@gmail.com>
2023-10-02 17:24:56 +0100

    IO landing section complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+
   root to: "pages#home"
+
+  get 'training', to: 'training#home', as: :training
+  get 'training/io', to: 'training#io', as: :io
+  get 'training/elara', to: 'training#elara', as: :elara
+  get 'training/studios', to: 'training#studios', as: :studios
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get 'training', to: 'pages#training', as: :training
   get 'about', to: 'pages#about', as: :about
   get 'blog', to: 'pages#blog', as: :blog
   get 'contact', to: 'pages#contact', as: :contact
